96d9eb37fb5249529ac17c40e3e3a7f8ac749003
RNT13 <renatornt13@gmail.com>
2025-08-08 20:51:45 -0300

    Update: - fix

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const API_BASE_URL = 'https://fake-api-tau.vercel.app/api/efood'
+const API_BASE_URL = 'https://ebac-fake-api.vercel.app/api/efood'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
